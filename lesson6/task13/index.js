1e042344ec6ba2133abe48dcf5d7c99fdd5e7dc8
Maslenykova <smaslenykova8@gmail.com>
2024-07-30 14:30:36 +0300

    change

--- lesson6/task13/index.js
+++ lesson6/task13/index.js
@@ -1,17 +1,13 @@
 function withdraw(clients, balances, client, amount) {
-    let newBalanse = [];
-  
-    for (let i = 0; i < clients.length; i += 1) {
-      if (clients[i] === client) {
-        balances[i] -=  amount;
-      }
-      newBalanse.push(balances[i]);
-      
+ 
+  for (let i = 0; i < clients.length; i += 1) {
+    if (balances[i] < amount) {
+      return (-1);
     }
-    return newBalanse;
-      
-     
+    if (balances[i] > amount && clients[i] === client) {
+      return (balances[i] - amount);
+    } 
   }
-  
+}
   console.log(withdraw(['Ann', 'John', 'User'], [1400, 87, -6], 'John', 50));
  console.log( withdraw(['Ann', 'John', 'User'], [1400, 87, -6], 'User', 10));
--- lesson6/task16/index.js
+++ lesson6/task16/index.js
@@ -2,15 +2,13 @@ function removeDuplicates(array) {
     if (!Array.isArray(array)) {
       return null;
     }
-    let newArray = [];
-    for (let i = 0; i < array.length; i+=1) {
-        for (let j = i + 1; j < array.length; j+=1) {
-          if (array[i] === array[j] ) {
-            newArray.push(array[i]);
-          }
-        }
-      }
-    return newArray;
+    let myArray = new Set();
+    let arr = [];
+    for( let i = 0; i < array.length; i += 1){
+      myArray.add(array[i]);
+      arr = [...myArray];
+    }
+    return arr;
   }
   console.log(removeDuplicates([1, 1, 2, 3, 4, 4, 6]));
   console.log(removeDuplicates([1, 5, 7, 1, 2, 3, 4, 4, 3, 6]));
