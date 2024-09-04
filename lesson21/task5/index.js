be04d36e3469e150167769d7f387e9627dcb84f2
Maslenykova <smaslenykova8@gmail.com>
2024-09-04 14:45:29 +0300

    change

--- lesson21/task5/index.js
+++ lesson21/task5/index.js
@@ -1,7 +1,7 @@
 export function setTitle(text){
  const title = document.querySelector('.title').textContent;
 document.querySelector('.title').textContent = text;
-let titleText = document.title.textContent;
+let titleText = document.querySelector('.title').textContent;
  return titleText; 
 }
 
