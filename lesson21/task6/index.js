95dfe4d512e297144211c362b885d0615948ebff
Maslenykova <smaslenykova8@gmail.com>
2024-09-05 17:02:44 +0300

    change

--- lesson21/task6/index.js
+++ lesson21/task6/index.js
@@ -1,9 +1,10 @@
 export function setButton(buttonText){
-    const elementBody = document.querySelector('.body');
-    const bodyButton = document.createElement('button');
-    bodyButton.innerHTML = buttonText;
-    elementBody.append(bodyButton);
+    const elementBody = document.querySelector('body');
+         const bodyButton = document.createElement('button');
+        bodyButton.innerHTML = buttonText;
+        elementBody.append(bodyButton);
     return elementBody;
 }
+
 
 console.log(setButton('button text'));
