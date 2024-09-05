d5ba53442874c509c069d0b6b9cc5f112417a59c
Maslenykova <smaslenykova8@gmail.com>
2024-09-05 21:54:57 +0300

    change

--- lesson21/task10/index.js
+++ lesson21/task10/index.js
@@ -1,14 +1,12 @@
 export function finishForm(){
-    const elem = document.querySelector('.login-form');
+    const finishForm = document.querySelector('.login-form');
     const inputElem = document.createElement('input');
     inputElem.setAttribute('type', 'text');
     inputElem.setAttribute('name', 'login');
     const passwordElem = document.querySelector('input');
     passwordElem.setAttribute('type', 'password');
-    if(elem){
-        elem.prepend(inputElem);
-    }
-    return elem;
+    finishForm.prepend(inputElem);
+    return finishForm;
 }
 
 console.log(finishForm());
