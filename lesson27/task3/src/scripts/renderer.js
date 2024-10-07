2904f3237fd052fc796b5da29731759f8ea1fcf1
Maslenykova <smaslenykova8@gmail.com>
2024-10-07 20:35:26 +0300

    change

--- lesson27/task3/src/scripts/renderer.js
+++ lesson27/task3/src/scripts/renderer.js
@@ -1,38 +1,44 @@
-import {getItem, setItem} from './storage.js';
+import { getItem, setItem } from "./storage.js";
 
 const listElem = document.querySelector('.list');
-const createCheckbox = document.createElement('input');
-const createBtn = document.querySelector('.create-task-btn');
 
-const compareTasks = (tasksList) =>{
-   tasksList
-  .sort((a, b) => a.done - b.done);
-  return tasksList; 
-}
+const createCheckbox = ({ done, id }) => {
+  const checkboxElem = document.createElement('input');
+  checkboxElem.setAttribute('type', 'checkbox');
+  checkboxElem.checked = done;
+  checkboxElem.dataset.id = id;
+  return checkboxElem;
+};
 
-const createListItem = ({ text, done, id}) => {
-    const listItemElem = document.createElement('li');
-      listItemElem.classList.add('list__item');
-      const checkboxElem = createCheckbox({done, id});
-      if (done) {
-        listItemElem.classList.add('list__item_done');
-      }
-      listItemElem.append(checkboxElem, text);
+const compareTasks = (a, b) => a.done - b.done;
 
-      return listItemElem;
-      }
+const createListItem = ({ text, done, id }) => {
+  const listItemElem = document.createElement('li');
+  listItemElem.classList.add('list__item');
 
+  const checkboxElem = createCheckbox({ done, id });
+
+  if (done) {
+    listItemElem.classList.add('list__item_done');
+  }
+
+  const textNode = document.createTextNode(text);
+  listItemElem.append(checkboxElem, textNode);
+
+  return listItemElem;
+};
 
 
 export const renderTasks = () => {
-    const tasksList = getItem('tasksList') || [];
+  const tasksList = getItem('tasksList'); 
 
-    listElem.innerHTML = '';
-    const tasksElems = tasksList
-    .sort(compareTasks)
-    .map(createListItem);
-    listElem.append(...tasksElems);
-    const newTasksList = listElem;
-    setItem('tasksList', newTasksList);
-}
-  
+  listElem.innerHTML = ''; 
+
+  const tasksElems = tasksList.sort(compareTasks).map(createListItem);
+
+  listElem.append(...tasksElems);
+  setItem('tasksList', tasksList);
+};
+
+
+renderTasks();
